74452599fa4d665bbcc5d9e0384f379bc3bb5db2
yokota26 <rpslym1026@gmail.com>
2025-04-14 07:26:45 +0900

    file move

--- js/main.js
+++ js/main.js
@@ -1,66 +1,87 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 import { OrbitControls } from "../build/jsm/OrbitControls.js";
 // import * as THREE from "../build/three.module.js";
 
 let scene, camera, renderer, pointLight, controls;
 
-// add scene
-scene = new THREE.Scene();
+window.addEventListener("load", init);
 
-// add camera
-camera = new THREE.PerspectiveCamera(
-  50,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+// init=初期化
+function init() {
+  // add scene
+  scene = new THREE.Scene();
 
-camera.position.set(0, 0, 500);
+  // add camera
+  camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-// add render
-renderer= new THREE.WebGLRenderer({
-  alpha: true // alpha: 透明,defaulte = false
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-// Where? > <body>
-document.body.appendChild(renderer.domElement);
+  camera.position.set(0, 0, 500);
 
-// texuture
-let texture = new THREE.TextureLoader().load("tutorial/textures/earth.jpg");
+  // add render
+  renderer = new THREE.WebGLRenderer({
+    alpha: true, // alpha: 透明,defaulte = false
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  // Where? > <body>
+  document.body.appendChild(renderer.domElement);
 
-// geometry
-let ballGeometry = new THREE.SphereGeometry(100, 64, 32) //(半径 + widesegment(polligonの数) + heightsegment)
+  // texuture
+  let texture = new THREE.TextureLoader().load("/tutorial/textures/earth.jpg");
 
-// material
-let ballMaterial = new THREE.MeshPhysicalMaterial({
-  // color: 
-  map: texture
-});
+  // geometry
+  let ballGeometry = new THREE.SphereGeometry(100, 64, 32); //(半径 + widesegment(polligonの数) + heightsegment)
 
-// geometry + material = mesh
-let ballMesh =  new THREE.Mesh(ballGeometry, ballMaterial);
-scene.add(ballMesh);
+  // material
+  let ballMaterial = new THREE.MeshPhysicalMaterial({
+    // color:
+    map: texture,
+  });
 
-// 並行光源(カラー＋強さ)
-let directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-directionalLight.position.set(1, 1, 1);
-// 0x = 16
-scene.add(directionalLight);
+  // geometry + material = mesh
+  let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
+  scene.add(ballMesh);
 
-// point光源
-pointLight = new THREE.PointLight(0xffffff, 2);
-pointLight.position.set(-200, -200, -200);
+  // 並行光源(カラー＋強さ)
+  let directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+  directionalLight.position.set(1, 1, 1);
+  // 0x = 16
+  scene.add(directionalLight);
 
-// 以下の２行をscene.add(pointLight)の前に追加
-pointLight.decay = 1;
-pointLight.power = 1000;
+  // point光源
+  pointLight = new THREE.PointLight(0xffffff, 2);
+  pointLight.position.set(-200, -200, -200);
 
-scene.add(pointLight);
+  // 以下の２行をscene.add(pointLight)の前に追加
+  pointLight.decay = 1;
+  pointLight.power = 1000;
 
-// where pointlight
-let pointLightHelper = new THREE.PointLightHelper(pointLight, 30); //第二引数は大きさ
+  scene.add(pointLight);
 
-scene.add(pointLightHelper);
+  // where pointlight
+  let pointLightHelper = new THREE.PointLightHelper(pointLight, 30); //第二引数は大きさ
+
+  scene.add(pointLightHelper);
+
+  window.addEventListener("resize", onWindowResize);
+  animate();
+
+  // orbitcontrols
+  controls = new OrbitControls(camera, renderer.domElement);
+}
+
+// resize 
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // camera aspect
+  camera.aspect = window.innerWidth/window.innerHeight;
+  camera.updateProjectionMatrix();
+}
 
 function animate() {
   // pointlight arownd
@@ -73,8 +94,3 @@ function animate() {
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
-
-// orbitcontrols
-controls = new OrbitControls(camera, renderer.domElement);
-
-animate();
